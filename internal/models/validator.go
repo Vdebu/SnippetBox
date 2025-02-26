0ed89e9ea77d4547cbc1390a9094b86a2527ffa9
Vdebu <Vdebuzzz@gmail.com>
2025-02-26 23:53:22 +0800

    为部分方法添加更详细的注释。

--- internal/models/validator.go
+++ internal/models/validator.go
@@ -63,6 +63,7 @@ func (v *Validator) MaxChars(value string, max int) bool {
 func PermittedValue[T comparable](val T, permittedValues ...T) bool {
 	for _, v := range permittedValues {
 		if val == v {
+
 			return true
 		}
 	}
